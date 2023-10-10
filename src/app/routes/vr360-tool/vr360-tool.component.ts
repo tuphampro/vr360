e3ee675bc9f18959efc5ae5fc368a0014b8b4fe6
tupv <tupv@vnpt.vn>
2023-10-10 14:35:40 +0700

    - Update code

--- src/app/routes/vr360-tool/vr360-tool.component.ts
+++ src/app/routes/vr360-tool/vr360-tool.component.ts
@@ -7,14 +7,7 @@ import { ChangeDetectionStrategy, Component, ElementRef, OnInit } from '@angular
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class VR360ToolComponent implements OnInit {
-  offsetX = 0;
-  offsetY = 0;
-  mousedown_offsetX = 0;
-  mousedown_offsetY = 0;
-  xAxis: ElementRef | undefined;
-  yAxis: ElementRef | undefined;
-  viewSource = false;
-  preview = true;
+  pannellumViewer: any;
 
   tabs = [
     {
@@ -28,19 +21,18 @@ export class VR360ToolComponent implements OnInit {
   ];
 
   ngOnInit() {
-    this.xAxis = new ElementRef(document.getElementById('x-axis'));
-    this.yAxis = new ElementRef(document.getElementById('y-axis'));
-
-    (window as any).pannellum.viewer('panorama', {
+    this.pannellumViewer = (window as any).pannellum.viewer('panorama', {
       default: {
         firstScene: 'circle',
         author: 'Matthew Petroff',
         sceneFadeDuration: 250
       },
+      hotSpotDebug: false,
       type: 'equirectangular',
       panorama: '../assets/images/3.jpg',
       autoLoad: true,
-      compass: true,
+      compass: true
+
       // "pitch": 2.3,
       // "yaw": -135.4,
       // "hfov": 120,
@@ -65,72 +57,60 @@ export class VR360ToolComponent implements OnInit {
       //     "text": "North Charles Street"
       //   }
       // ],
-      scenes: {
-        circle: {
-          title: 'Mason Circle',
-          hfov: 110,
-          pitch: -3,
-          yaw: 117,
-          type: 'equirectangular',
-          panorama: '/images/from-tree.jpg',
-          hotSpots: [
-            {
-              pitch: -2.1,
-              yaw: 132.9,
-              type: 'scene',
-              text: 'Spring House or Dairy',
-              sceneId: 'house'
-            }
-          ]
-        },
+      // scenes: {
+      //   circle: {
+      //     title: 'Mason Circle',
+      //     hfov: 110,
+      //     pitch: -3,
+      //     yaw: 117,
+      //     type: 'equirectangular',
+      //     panorama: '/images/from-tree.jpg',
+      //     hotSpots: [
+      //       {
+      //         pitch: -2.1,
+      //         yaw: 132.9,
+      //         type: 'scene',
+      //         text: 'Spring House or Dairy',
+      //         sceneId: 'house'
+      //       }
+      //     ]
+      //   },
 
-        house: {
-          title: 'Spring House or Dairy',
-          hfov: 110,
-          yaw: 5,
-          type: 'equirectangular',
-          panorama: '/images/bma-0.jpg',
-          hotSpots: [
-            {
-              pitch: -0.6,
-              yaw: 37.1,
-              type: 'scene',
-              text: 'Mason Circle',
-              sceneId: 'circle',
-              targetYaw: -23,
-              targetPitch: 2
-            }
-          ]
-        }
-      }
+      //   house: {
+      //     title: 'Spring House or Dairy',
+      //     hfov: 110,
+      //     yaw: 5,
+      //     type: 'equirectangular',
+      //     panorama: '/images/bma-0.jpg',
+      //     hotSpots: [
+      //       {
+      //         pitch: -0.6,
+      //         yaw: 37.1,
+      //         type: 'scene',
+      //         text: 'Mason Circle',
+      //         sceneId: 'circle',
+      //         targetYaw: -23,
+      //         targetPitch: 2
+      //       }
+      //     ]
+      //   }
+      // }
     });
+    this.pannellumViewer.on('mouseup', this.mouseupViewer);
   }
 
-  onMouseMove(ev: MouseEvent) {
-    this.offsetX = ev.offsetX;
-    this.offsetY = ev.offsetY;
+  mouseupViewer = (event: MouseEvent) => {
+    try {
+      var inputText = 'Tu Pham';
 
-    if (this.offsetX < 0) this.offsetX = 0;
-    if (this.offsetY < 0) this.offsetY = 0;
+      // coords[0] is pitch, coords[1] is yaw
+      var coords = this.pannellumViewer.mouseEventToCoords(event);
 
-    // this.xAxis!.nativeElement.style.top = `${this.offsetY}px`;
-    // this.xAxis!.nativeElement.style.left = 0;
-
-    // this.yAxis!.nativeElement.style.top = 0;
-    // this.yAxis!.nativeElement.style.left = `${this.offsetX}px`;
-  }
-
-  onMouseDown(ev: MouseEvent) {
-    this.mousedown_offsetX = ev.offsetX;
-    this.mousedown_offsetY = ev.offsetY;
-  }
-
-  onViewSource() {
-    this.viewSource = true;
-    this.preview = false;
-  }
-  onPreview() {
-    this.viewSource = false;
-    this.preview = true;
-  }
+      var currentPitch = coords[0];
+      var currentYaw = coords[1];
+      this.pannellumViewer.addHotSpot({ pitch: currentPitch, yaw: currentYaw, type: 'info', text: inputText });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 }
